6fcdc33105c8757687692be810b0d4baa3080aac
fredi4302 <fredigaming4302@gmail.com>
2023-10-05 10:25:19 +0530

    Update openapi.py
    imported the os library to create environment variables

--- Guidelines/openapi.py
+++ Guidelines/openapi.py
@@ -1,4 +1,8 @@
 import openapi
+import os
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
 
 #created api key
 openapi.api_key="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"
