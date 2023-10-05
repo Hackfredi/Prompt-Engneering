c59097d2c8659234ff2b13876c3adc125e0432db
Hackfredi <121596453+Hackfredi@users.noreply.github.com>
2023-10-05 10:48:38 +0530

    Update openapi.py
    Removed api key

--- Guidelines/openapi.py
+++ Guidelines/openapi.py
@@ -5,4 +5,4 @@ from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 
 #created api key
-openapi.api_key="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"
+openapi.api_key="sk-"
